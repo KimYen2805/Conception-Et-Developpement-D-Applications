5dcf28e37dbdef2c268ffc774bdf2a6d9ecbdbac
Kim Yen <lekkimyen@gmailcom>
2024-03-05 09:45:39 +0100

    ...

---
+++ src/Joueur.h
@@ -0,0 +1,17 @@
+#ifndef _JOUEUR_H
+#define _JOUEUR_H
+#include <string>
+using namespace std;
+class Joueur{
+    string nom;        // Nom du personnage
+    int pointsDeVie;        // Points de vie du personnage
+    int puissanceAttaque;   // Puissance d'attaque du personnage
+
+    
+}
+#endif
+
+
+
+
+

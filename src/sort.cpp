8008c1080ee7dd15f85772d8bdc662a9ef392642
Kim Yen <lekkimyen@gmailcom>
2024-03-10 23:53:40 +0100

    modified:   src/sort.cpp

--- src/sort.cpp
+++ src/sort.cpp
@@ -42,22 +42,3 @@ Effet Sort::getEffetSort() const
 {
     return effetSort;
 }
-void Sort::testRegression() {
-    cout << "Exécution du test de régression pour la classe Sort..." << endl;
-
-    // Test 1: Vérifiez si le nom du sort est "Agidyne"
-    if (getNomSort() == "Agidyne") {
-        cout << "Test 1 réussi : Le nom du sort est \"Agidyne\"." << endl;
-    } else {
-        cout << "Test 1 échoué : Le nom du sort n'est pas \"Agidyne\"." << endl;
-    }
-
-    // Test 2: Vérifiez si le coût en mana est égal à 7
-    if (getCoutMana() == 7) {
-        cout << "Test 2 réussi : Le coût en mana est égal à 7." << endl;
-    } else {
-        cout << "Test 2 échoué : Le coût en mana n'est pas égal à 7." << endl;
-    }
-
-    cout << "Test de régression terminé." << endl;
-}

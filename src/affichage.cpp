4215bbfd430cd729c074bea5704947929f0963be
Kim Yen <lekkimyen@gmailcom>
2024-04-05 21:11:05 +0200

    aff 	modified:   src/affichage.cpp 	modified:   src/affichage.h 	modified:   src/mainAffichage.cpp

--- src/affichage.cpp
+++ src/affichage.cpp
@@ -44,7 +44,11 @@ if (renderer == nullptr)
     cerr<<"TTF_Init : "<<TTF_GetError()<<endl;
     exit(2);
 	}
-	font = TTF_OpenFont("data/Arial.ttf", 17);
+font = TTF_OpenFont("data/Arial.ttf", 17);
+    if (font == nullptr)
+        {
+        font ==TTF_OpenFont("data/Arial.ttf", 17);
+        }
     if (font == nullptr) {
         cerr << "Échec du chargement de la police:  " << TTF_GetError() << endl;
         SDL_Quit();
@@ -54,6 +58,14 @@ if (renderer == nullptr)
 	// Initialisation des couleurs 
     textColor = {255, 255, 255};
     rectColor = {241, 196, 15};
+    // saisie de texte
+    inputRect.x = 0;
+    inputRect.y = 0;
+    inputRect.w = 0;
+    inputRect.h = 0;
+
+    // Khởi tạo inputText
+   // inputText = "";
 }
 
 /**
@@ -62,9 +74,33 @@ if (renderer == nullptr)
 Affichage::~Affichage()
 {
 
-  TTF_CloseFont(font); // Libère la police
+    TTF_CloseFont(font); // Libère la police
+
     SDL_DestroyRenderer(renderer); // Libère le renderer
     SDL_DestroyWindow(window); // Libère la fenêtre
+    //
+    SDL_DestroyTexture(textureImage1);
+    SDL_FreeSurface(image1);
+
+    SDL_DestroyTexture(textureImage2);
+    SDL_FreeSurface(image2);
+
+    SDL_DestroyTexture(textureImageFond);
+    SDL_FreeSurface(imageFond);
+
+   SDL_FreeSurface( tempSurface);
+
+   SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(textSurface);//
+
+     SDL_DestroyTexture(textureImageBouton);
+    SDL_FreeSurface(imageBouton);
+    
+    SDL_DestroyTexture(textInputTexture);
+    SDL_FreeSurface(textInputSurface);
+
+   
+
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
@@ -76,29 +112,29 @@ void Affichage::dessinerPersonnage()
 	int posXJoueur= 350; 
 	int posYJoueur= 320;
 	// Affichage de l'image ennemi
-        SDL_Surface *image1= IMG_Load("data/ennemi.png");
+        image1= IMG_Load("data/ennemi.png");
       if (!image1)
       {
          cerr << "Erreur de chargement de l'image: " << SDL_GetError() << endl;
         }
-        SDL_Texture *textureImage1= SDL_CreateTextureFromSurface(renderer, image1);
-        SDL_FreeSurface(image1);
+        textureImage1= SDL_CreateTextureFromSurface(renderer, image1);
+      //  SDL_FreeSurface(image1);
 		//Positionnement et affichage d'image ennemi
 		SDL_Rect posIma1;
 		posIma1.x = 100; 
 		posIma1.y= 100;
 		SDL_QueryTexture(textureImage1, NULL, NULL, &posIma1.w, &posIma1.h);
 		SDL_RenderCopy(renderer, textureImage1,NULL, &posIma1);
-
+        //SDL_DestroyTexture(textureImage1);
 
 		//affichage de l'image joueur 
-		SDL_Surface *image2= IMG_Load("data/joueur.png");
+		image2= IMG_Load("data/joueur.png");
 		if ( !image2)
 		{
 			cerr<< "Erreur de chargement de l'image: " << SDL_GetError() << endl;
 		}
-		SDL_Texture *textureImage2 = SDL_CreateTextureFromSurface(renderer, image2);
-		SDL_FreeSurface(image2);
+		textureImage2 = SDL_CreateTextureFromSurface(renderer, image2);
+		//SDL_FreeSurface(image2);
 		
 		//Positionnement et affichage d'image 2 joueur
 	SDL_Rect posIma2;
@@ -106,13 +142,15 @@ void Affichage::dessinerPersonnage()
 		posIma2.y= posYJoueur;
 	SDL_QueryTexture(textureImage2, NULL, NULL, &posIma2.w, &posIma2.h);
 	SDL_RenderCopy(renderer, textureImage2,NULL, &posIma2);
+  //  SDL_DestroyTexture(textureImage2);
+
 }
 void Affichage:: barres(Joueur joueur)
 {
 	string nomDeJoueur = joueur.getNomJoueur();
 	int pointDeVieJoueur = joueur.getPVJoueur(); // Point de vie du joueur
     int maxManaJoueur = joueur.getMana()*100/joueur.getMAXMana();    // Mana maximale du joueur
-	   Ennemi ennemi;
+	Ennemi ennemi;
     int pointDeVieEnnemi= ennemi.getPointDeVieEnnemi(); 
 // une barre (pointDVactu)
 	SDL_Rect rePo; 
@@ -137,11 +175,11 @@ void Affichage:: barres(Joueur joueur)
 larMax = 0; //stocker la largeur max des textes
 hautTotale = 0;
 for (const auto& line : li) {
-    SDL_Surface* tempSurface = TTF_RenderText_Blended(font, line.c_str(), textColor);
+     tempSurface = TTF_RenderText_Blended(font, line.c_str(), textColor);
     larMax = max(larMax, tempSurface->w); //Mettre à jour la largeur max 
     hautTotale += tempSurface->h; //Ajouter la hauteur de cette ligne à la hauteur totale 
     haut.push_back(tempSurface->h); //Stoker la hauteur de cette ligne
-    SDL_FreeSurface(tempSurface);
+   // SDL_FreeSurface(tempSurface);
 }
 // Ajouter un espacement entre les lignes
 int spacing = 5;
@@ -162,13 +200,13 @@ SDL_RenderFillRect(renderer, &rect);
 int posY =10;
 int spacing = 5;
 for (size_t i = 0; i < lignes.size(); ++i) {
-        SDL_Surface* textSurface = TTF_RenderText_Blended(font, lignes[i].c_str(), textColor);
-        SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+         textSurface = TTF_RenderText_Blended(font, lignes[i].c_str(), textColor);
+         textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
         SDL_Rect textRect = {310, posY, textSurface->w, textSurface->h}; // Position avec marge
         SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
         posY += textSurface->h + spacing;
-        SDL_FreeSurface(textSurface);
-        SDL_DestroyTexture(textTexture);
+       // SDL_FreeSurface(textSurface);
+       // SDL_DestroyTexture(textTexture);
 
     }
 	//Réinitialiser la couleur du renderer pour ne pas affecter les dessins
@@ -190,36 +228,36 @@ void Affichage::AfficherInfo(Joueur joueur, Ennemi ennemi) {
 //
 void Affichage::AfficherFond()
 {
-    SDL_Surface *imageFond= IMG_Load("data/fond.jpg");
+    imageFond= IMG_Load("data/fond.jpg");
 		if ( !imageFond)
 		{
 			cerr<< "Erreur de chargement de l'image: " << SDL_GetError() << endl;
 		}
-		SDL_Texture *textureImageFond = SDL_CreateTextureFromSurface(renderer, imageFond);
-		SDL_FreeSurface(imageFond);
+		textureImageFond = SDL_CreateTextureFromSurface(renderer, imageFond);
+	//	SDL_FreeSurface(imageFond);
 		
 		//Positionnement et affichage d' imageBouton
 		posFond.x = 0; 
 		posFond.y= 0;
 	SDL_QueryTexture(textureImageFond, NULL, NULL, &posFond.w, &posFond.h);
 	SDL_RenderCopy(renderer, textureImageFond,NULL, &posFond);
+  //  SDL_DestroyTexture(textureImageFond);
 }
 //
 void Affichage::AfficherBoutonAction()
 {
-    SDL_Surface *imageBouton= IMG_Load("data/bouton.png");
-		if ( !imageBouton)
-		{
-			cerr<< "Erreur de chargement de l'image: " << SDL_GetError() << endl;
-		}
-		SDL_Texture *textureImageBouton = SDL_CreateTextureFromSurface(renderer, imageBouton);
-		SDL_FreeSurface(imageBouton);
-		
-		//Positionnement et affichage d' imageBouton
-		posBouton.x = 230; 
-		posBouton.y= 200;
-	SDL_QueryTexture(textureImageBouton, NULL, NULL, &posBouton.w, &posBouton.h);
-	SDL_RenderCopy(renderer, textureImageBouton,NULL, &posBouton);
+   if (!boutonClique) {
+        imageBouton = IMG_Load("data/bouton.png");
+        if (!imageBouton) {
+            cerr << "Erreur de chargement de l'image: " << SDL_GetError() << endl;
+        }
+        textureImageBouton = SDL_CreateTextureFromSurface(renderer, imageBouton);
+        //Positionnement et affichage d' imageBouton
+        posBouton.x = 230;
+        posBouton.y = 200;
+        SDL_QueryTexture(textureImageBouton, NULL, NULL, &posBouton.w, &posBouton.h);
+        SDL_RenderCopy(renderer, textureImageBouton, NULL, &posBouton);
+    }
 }
 void Affichage::HandleMouseClick(SDL_Event event)
 {
@@ -232,22 +270,132 @@ void Affichage::HandleMouseClick(SDL_Event event)
         if (mouseX >= posBouton.x && mouseX <= posBouton.x + posBouton.w &&
             mouseY >= posBouton.y && mouseY <= posBouton.y + posBouton.h) {
             // le code de traitement lorsque le bouton est cliqué
+            boutonClique = true;
             // Par exemple : ouvrir une nouvelle fenêtre 
             cout << "Le bouton a été cliqué !" << std::endl;
         }
     }
 }
+//saisie le text
+ /*void Affichage::InitTextInput()
+ {
+    inputRect.x = 10;
+    inputRect.y = 10;
+    inputRect.w = 200; // Largeur ajustée pour plus de texte
+    inputRect.h = 30; 
+    SDL_StartTextInput();
+    SDL_SetTextInputRect(&inputRect);
+ }
+void Affichage::DessinerTextInput() {
+    // Dessiner le rectangle de la zone de saisie
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &inputRect);
 
+    // Dessiner le texte entré
+    SDL_Surface* textSurface = TTF_RenderText_Blended(font, inputText.c_str(), textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_FreeSurface(textSurface);
+
+    SDL_Rect textRect = {inputRect.x + 5, inputRect.y + 5, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+    SDL_DestroyTexture(textTexture);
+    //renderTextInput(inputText, inputRect.x, inputRect.y);
+}*/
+/*void Affichage::HandleTextInputEvent(SDL_Event event) {
+    if (event.type == SDL_TEXTINPUT) {
+        inputText += event.text.text;
+    } else if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.scancode == SDL_SCANCODE_BACKSPACE && !inputText.empty()) {
+            inputText.pop_back();
+        } else if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
+            // Traitement du texte entré ici, par exemple :
+            std::cout << "Texte entré : " << inputText << std::endl;
+            inputText.clear(); // Clear input text after enter
+        }
+    }
+}*/
+
+void Affichage::DessinerTextInput() {
+    bool quit = false;
+    SDL_Event event;
+    SDL_StartTextInput();
+
+    std::string inputText = ""; // Giả sử bạn đã khai báo và khởi tạo đúng
+    //SDL_Color textColor = {255, 255, 255, 255}; // Màu trắng
+
+    // While application is running
+    while (!quit) {
+        // The rerender text flag
+        bool renderText = false;
+
+        // Handle events on queue
+        while (SDL_PollEvent(&event) != 0) {
+            // User requests quit
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+            // Special key input
+            else if (event.type == SDL_KEYDOWN) {
+                // Handle backspace
+                if (event.key.keysym.sym == SDLK_BACKSPACE && inputText.length() > 0) {
+                    // Lop off character
+                    inputText.pop_back();
+                    renderText = true;
+                }
+                // Handle copy
+                else if (event.key.keysym.sym == SDLK_c && SDL_GetModState() & KMOD_CTRL) {
+                    SDL_SetClipboardText(inputText.c_str());
+                }
+                // Handle paste
+                else if (event.key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL) {
+                    inputText = SDL_GetClipboardText();
+                    renderText = true;
+                }
+            }
+            //Handle text input
+            else if (event.type == SDL_TEXTINPUT) {
+                // Not copy or pasting
+                if (!(SDL_GetModState() & KMOD_CTRL && (event.text.text[0] == 'c' || event.text.text[0] == 'C' || event.text.text[0] == 'v' || event.text.text[0] == 'V'))) {
+                    // Append character
+                    inputText += event.text.text;
+                    renderText = true;
+                }
+            }
+        }
+
+        // Render new text
+        if (renderText && !inputText.empty()) {
+            // Free the existing textures if necessary
+            //SDL_DestroyTexture(textInputTexture);
+
+            // Create a surface from the text
+           textInputSurface = TTF_RenderText_Blended(font, inputText.c_str(), textColor);
+            textInputTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+            SDL_Rect textRect = {10, 10, textSurface->w, textSurface->h};
+          //  SDL_FreeSurface(textSurface); // No longer need the surface
+
+            // Clear the screen
+            SDL_RenderClear(renderer);
+
+            // Render the text
+            SDL_RenderCopy(renderer, textInputTexture, NULL, &textRect);
+
+            // Update the screen
+           // SDL_RenderPresent(renderer);
+        }
+    }
+
+    SDL_StopTextInput();
+  //  SDL_DestroyTexture(textInputTexture); // Cleanup
+}
 void Affichage::GererEvenements() {
     SDL_Event events;
-	//
-	//SDL_Rect boutonRect;
-    //InitialiserRectBoutonAction(boutonRect);
     // Gestion evenements
     while (SDL_PollEvent(&events)) {
         if (events.type == SDL_QUIT) {
             SDL_Quit();
-            exit(0); // Thoát khỏi chương trình hoàn toàn khi nhấn nút thoát
+            exit(0); 
         } else if (events.type == SDL_KEYDOWN) {
             switch (events.key.keysym.scancode) {
                 case SDL_SCANCODE_ESCAPE:
@@ -266,29 +414,26 @@ void Affichage::GererEvenements() {
     }
 }
 
-void Affichage::AfficherJeu(Joueur joueur, Ennemi ennemi,Jeu jeu) {
-    bool quit = false;
-    //Initialisez le rec pour le bouton 
-    while (!quit) {
+void Affichage::AfficherJeu(Joueur joueur, Ennemi ennemi, Jeu jeu) {
+    bool quitter = false;
+
+    while (!quitter) {
         GererEvenements();
-        // Effacement de l'écran
-        SDL_SetRenderDrawColor(renderer, 191, 201, 202, 255);
-        SDL_RenderClear(renderer);
-		//Font
-		AfficherFond();
+        // Affichage des éléments du jeu
+        AfficherFond();
         dessinerPersonnage(); 
         barres(joueur); 
         AfficherInfo(joueur, ennemi);
-		// Afficher le bouton "action"
         AfficherBoutonAction();
-		
+   // DessinerTextInput();
         // Affichage à l'écran
         SDL_RenderPresent(renderer);
-        SDL_Delay(10); // Trì hoãn vòng lặp một chút để giảm tải CPU
+
+        // Vérification si l'utilisateur veut quitter
+        SDL_Delay(4); 
     }
 }
-	//entrer le texte pour gerer le tableau de sort
-				
 
+				
 			
 			
--- src/affichage.h
+++ src/affichage.h
@@ -13,13 +13,40 @@
 class Affichage {
         private:
         SDL_Window* window;
-        SDL_Renderer* renderer;
-        TTF_Font *font;
-        SDL_Color textColor;
-         SDL_Color rectColor;
-         SDL_Rect posBouton;
-         SDL_Rect posFond;
+    SDL_Renderer* renderer;
 
+    SDL_Texture* textureImage1;
+    SDL_Surface *image1;//
+
+    SDL_Texture* textureImage2;
+    SDL_Surface *image2;//
+
+    SDL_Texture* textureImageFond;
+    SDL_Surface *imageFond;//
+
+    SDL_Surface* tempSurface;//
+
+    SDL_Texture* textTexture;//
+    SDL_Surface* textSurface ;
+
+    SDL_Rect posFond;
+   
+
+    SDL_Rect posBouton;
+    SDL_Surface *imageBouton;
+    SDL_Texture *textureImageBouton;
+    bool boutonClique = false;
+    //
+
+    TTF_Font* font;//
+
+    SDL_Color textColor;
+    SDL_Color rectColor;
+
+    SDL_Rect inputRect;
+    std::string inputText;
+     SDL_Surface* textInputSurface ;
+ SDL_Texture* textInputTexture;//
  
     
         public:
@@ -34,7 +61,7 @@ class Affichage {
 /**
  * @brief Affiche l'image dans la fenêtre
 */
-         
+          void AfficherFond();
        
          void dessinerPersonnage();
         void barres(Joueur j);
@@ -42,16 +69,22 @@ class Affichage {
         void CalculerDimensionsDuTexte(const vector<string>& lignes, int& largeurMax, int& hauteurTotale, vector<int>& hauteurs);
         void DessinerFondTexte(SDL_Renderer* renderer,int largeurMax, int hauteurTotale);
         void AfficherTexte(SDL_Renderer* renderer,const vector<string>& lignes);
-      //  void AffichageTexte(SDL_Renderer* rend);
         void AfficherInfo(Joueur j, Ennemi enne);
+        //jeu 
         void AfficherJeu(Joueur j, Ennemi ennemi,Jeu jeu);
         void GererEvenements();
 
         void AfficherBoutonAction();
         void HandleMouseClick(SDL_Event event);
-        void InitialiserRectBoutonAction();
-        void Action(Joueur joueur);
-        void AfficherFond();
+        //void InitialiserRectBoutonAction();
+      //  void Action(Joueur joueur);
+       //gérer la boîte de texte
+       // Déclaration de la fonction InitTextInputPosition
+   // SDL_Rect InitTextInputPosition(int x, int y, int width, int height);
+      //void InitTextInput();
+    void DessinerTextInput();
+       //void HandleTextInputEvent(SDL_Event event);
+        //
 };
 
 #endif
